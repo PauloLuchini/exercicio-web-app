4c919669d286045ef2b472c09398758e706f7686
PauloLuchini <pluchini05@gmail.com>
2023-09-04 14:32:21 -0300

    Criando arquivo js com a lista de aparelhos

---
+++ src/listaAparelhos.js
@@ -0,0 +1,17 @@
+export const listaAparelhos = [
+    {
+        id : 1,
+        nome : 'Celular',
+        preco: 2000
+    },
+    {
+        id : 2,
+        nome : 'Tablet',
+        preco: 4000
+    },
+    {
+        id : 3,
+        nome : 'Notebook',
+        preco : 6000
+    }
+]
